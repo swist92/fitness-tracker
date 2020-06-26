a2c3aeeae03d8ba56494fad35d2ec473fc2605f8
Sarah Wist <sarahwist107@gmail.com>
2020-06-25 23:40:25 -0700

    created app.use and app.get functions

--- server.js
+++ server.js
@@ -7,6 +7,35 @@ const Workout = require("./models.workouts");
 const PORT = process.env.PORT || 3030;
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout",{
-    useNewUrlParser: true;
+    useNewUrlParser: true
+});
+
+const app = express;
+
+app.use(logger("dev"));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+app.get("/exercise", (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'exercise.html'));
+});
+
+app.get("/stats", (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'stats.html'));
+});
+
+app.post("/api/workouts", (req, res) => {
+    Workout.create({
+        day: new Date()
+    })
+        .then((data) => res.json(data))
+        .catch(event => console.error(event))
 });
 
